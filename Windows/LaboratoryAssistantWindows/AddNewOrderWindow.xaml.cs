95139bd8bf58f120052f8bc7bb62f199381a3f38
local <local@NINKEAR>
2025-09-16 20:02:54 +0500

    Добавьте файлы проекта.

---
+++ Windows/LaboratoryAssistantWindows/AddNewOrderWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MedLab.Windows.LaboratoryAssistantWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNewOrderWindow.xaml
+    /// </summary>
+    public partial class AddNewOrderWindow : Window
+    {
+        MedLabEntities medLabEntities = new MedLabEntities();
+        // Сделать возможность выбора сразу нескольких анализов в заказ
+        User selectedUser;
+        Order newOrder;
+        public AddNewOrderWindow(User User)
+        {
+            InitializeComponent();
+            try
+            {
+                ListService.ItemsSource = medLabEntities.Services.ToList();
+                selectedUser = User;
+                newOrder = new Order()
+                {
+                    DateOfCreation = DateTime.Now,
+                    IdStatusOrder = 2,
+                    IdUser = selectedUser.Id
+                };
+                medLabEntities.Orders.Add(newOrder);
+                medLabEntities.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }    
+        }
+
+        private void AddNewService_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedService = ListService.SelectedItem as Service;
+            AddBarcode addBarcode = new AddBarcode(newOrder, selectedService);
+            addBarcode.Show();
+        }
+
+    }
+}
